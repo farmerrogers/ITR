0169817771f055afedab6c78720666983884322f
Ben Rogers <rogers@farmerrogers.com>
2013-03-19 11:14:27 -0700

    First Class

---
+++ class01/lab1.rb
@@ -0,0 +1,10 @@
+2+2
+2-3
+2*3
+4.5+7
+x = 11
+y = 2
+puts x/y 
+first_name = "Ben"
+last_name = "Rogers"
+puts first_name + " " + last_name
---
+++ class01/myfirstscript.rb
@@ -0,0 +1,5 @@
+puts "Hello, World"
+first= "Hari"
+last= "Kahlsa"
+age= "11"
+puts first+ " " +last+ " " "is not" " " +age
